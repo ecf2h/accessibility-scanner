005d5b43d302cdea9f55f5432839b3e132836011
ecf2h <ecf2h@virginia.edu>
2018-03-25 01:48:00 -0400

    Initial commit

---
+++ spec/accessibility-scanner-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AccessibilityScannerView from '../lib/accessibility-scanner-view';
+
+describe('AccessibilityScannerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
